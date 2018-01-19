8b46bb17859dd71fa62a5eed73a3c85307b714e1
nricardo <nelson.s.ricardo@gmail.com>
2018-01-19 14:16:32 +0000

    Just minor tweaks...

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 __install() {
 	# list of dotfiles
 	local DOTFILES=(
-		".dircolors/"
+		".dircolors"
 		".bash_aliases"
 		".bash_logout"
 		".bash_prompt"
@@ -13,16 +13,22 @@ __install() {
 		".npmrc"
 	);
 
+	# enter user's home directory
+	cd ${HOME}
+
 	# create links on user's home
 	for f in ${DOTFILES[@]}; do
-		if [ -e ~/.dotfiles/${f} ]; then
-			ln -nfsv ~/.dotfiles/${f} ~/${f}
+		if [ -e ${HOME}/.dotfiles/${f} ]; then
+			ln -nfsv ${HOME}/.dotfiles/${f} ${f}
 		fi
 	done
+
+	# go back
+	cd -
 }
 
 # check if ".dotfiles" folder exists
-if [ ! -d ~/.dotfiles/ ]; then
+if [ ! -d ${HOME}/.dotfiles/ ]; then
 	echo 'No ".dotfiles" folder detected! Aborting...';
 	exit 1;
 fi
