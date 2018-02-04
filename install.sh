7ce635115126bd7692973908f6913918b20922ae
nricardo <nelson.s.ricardo@gmail.com>
2018-02-04 02:09:12 +0000

    Add symbol for path separation.

--- install.sh
+++ install.sh
@@ -13,16 +13,22 @@ __install() {
 		".npmrc"
 	);
 
+	# change to user's home folder
+	cd $HOME
+
 	# create links on user's home
 	for f in ${DOTFILES[@]}; do
-		if [ -e ~/.dotfiles/${f} ]; then
-			ln -nfsv ~/.dotfiles/${f} ~/${f}
+		if [ -e ${HOME}/.dotfiles/${f} ]; then
+			ln -nfsv ${HOME}/.dotfiles/${f} ${f}
 		fi
 	done
+
+	# go back to where we where
+	cd -
 }
 
 # check if ".dotfiles" folder exists
-if [ ! -d ~/.dotfiles/ ]; then
+if [ ! -d ${HOME}/.dotfiles/ ]; then
 	echo 'No ".dotfiles" folder detected! Aborting...';
 	exit 1;
 fi
